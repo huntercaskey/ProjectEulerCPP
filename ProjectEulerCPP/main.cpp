106df35629c9a6471419f96d215f89be839c67d1
huntercaskey <hunterrcaskey@gmail.com>
2016-08-11 16:08:34 -0400

    Initial Commit

---
+++ ProjectEulerCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProjectEulerCPP
+//
+//  Created by Hunter Caskey on 8/11/16.
+//  Copyright © 2016 Hunter Caskey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
